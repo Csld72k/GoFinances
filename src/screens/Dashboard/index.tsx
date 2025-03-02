90ad5f93ce55ca1662b9d1fe3f5f149ea7c87e5c
Csld72k <csld72k@gmail.com>
2025-03-02 19:36:03 -0300

    :construction: feat: add styled components and add the component Dashboard

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Tittle } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Tittle>My dashboard</Tittle>
+    </Container>
+  )
+}
