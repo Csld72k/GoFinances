12c5da4ede5a5a6a20aab0c8f38f5defaaed8d8b
Csld72k <csld72k@gmail.com>
2025-07-28 19:30:58 -0300

    :construction: Start creating header on homepage using responsive proportion

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles';
+
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/Csld72k.png' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Claudiney</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+
+      </Header>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,18 +1,55 @@
 //  For some reason here I'm facing a problem with type "O elemento de associação 'theme' tem implicitamente um tipo 'any'.ts(7031)"
 
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }: { theme: any }) => theme.colors.background};
 
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.regular};
-  font-size: 24px;
-  color: ${({ theme }: { theme: any }) => theme.colors.title};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+
+  background-color: ${({ theme }: { theme: any }) => theme.colors.primary};
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  flex-direction: row;
+  /* justify-content: space-between; */
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+flex-direction: row;
+align-items: center;
+gap: 17px;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+
+  border-radius: 50px;
+`;
+
+export const User = styled.View``;
+
+export const UserGreeting = styled.Text`
+color: ${({ theme }: { theme: any }) => theme.colors.shape};
+font-size: ${RFValue(18)}px;
+font-family: ${({ theme }: { theme: any }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+color: ${({ theme }: { theme: any }) => theme.colors.shape};
+font-size: ${RFValue(18)}px;
+font-family: ${({ theme }: { theme: any }) => theme.fonts.bold};
 `;
